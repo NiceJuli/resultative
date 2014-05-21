d80e6c5a421700effeb452f4b0869667ead31655
nicejuli <juli.vg@ya.ru>
2014-05-22 01:22:10 +0600

    второй блок и немного не доделанный четвертый

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 $(function() {
 	initSochiLayout();
+	switchBenefits();
 });
 
 $(window).resize(function(event) {
@@ -7,9 +8,19 @@ $(window).resize(function(event) {
 });
 
 function initSochiLayout() {
-	if($(window).height() > 750) {
+	if($(window).height() > 850) {
 		$('.b-first-box').css({height: $(window).height()-$('.b-header').outerHeight()});
 	} else {
-		$('.b-first-box').css({height: 750 - $('.b-header').outerHeight() + 'px'});
+		$('.b-first-box').css({height: 850 - $('.b-header').outerHeight() + 'px'});
 	}
+}
+
+function switchBenefits(){
+	$('.b-switch__item').click(function() {
+		var name = $(this).data('name');
+		$('.b-switch__item').removeClass('active');
+		$(this).addClass('active');
+		$('.b-material-info').removeClass('active');
+		$('.b-material-info').filter('.'+name).addClass('active');
+	});
 }
